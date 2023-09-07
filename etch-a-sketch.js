e2cd051926ec598011d44f359f4ff0aa8d920420
Maddie <nowosimm@mail.uc.edu>
2023-09-07 13:07:59 -0400

    color buttons

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,18 +1,96 @@
-document.addEventListener("DOMContentLoaded", function(){
-    createGraph(16)
+let color = "black"
 
-    
+document.addEventListener("DOMContentLoaded", function () {
+
+    // creating small/medium/large/reset buttons
+
+    const smallButton = document.createElement('button');
+    smallButton.textContent = "Small"
+    smallButton.onclick = function () {
+        smallGrid()
+    }
+    document.body.appendChild(smallButton)
+
+    const mediumButton = document.createElement('button');
+    mediumButton.textContent = "Medium"
+    mediumButton.onclick = function () {
+        mediumGrid()
+    }
+    document.body.appendChild(mediumButton)
+
+    const largeButton = document.createElement('button');
+    largeButton.textContent = "Large"
+    largeButton.onclick = function () {
+        largeGrid()
+    }
+    document.body.appendChild(largeButton)
+
+    const resetButton = document.createElement('button');
+    resetButton.textContent = "Reset"
+    document.body.appendChild(resetButton)
 })
 
-function createGraph(size){
+// creates boards for 3 sizes
+
+function smallGrid() {
     let graph = document.querySelector(".container");
-    graph.style.gridTemplateColumns = "repeat(16, 1fr)";
-    graph.style.gridTemplateRows = `repeat(16, 1fr)`;
+    graph.innerHTML = "";
+    graph.style.gridTemplateColumns = `repeat(45, 1fr)`;
+    graph.style.gridTemplateRows = `repeat(45, 1fr)`;
 
-    let numDivs = size * size;
-
-    for(let i = 0; i < numDivs; i++){
+    for (let i = 0; i < 45 * 45; i++) {
         let div = document.createElement("div");
         graph.insertAdjacentElement("beforeend", div);
+        div.addEventListener("mouseover", colorDiv)
     }
 }
+function mediumGrid() {
+    let graph = document.querySelector(".container");
+    graph.innerHTML = "";
+    graph.style.gridTemplateColumns = `repeat(30, 1fr)`;
+    graph.style.gridTemplateRows = `repeat(30, 1fr)`;
+  
+    for (let i = 0; i < 30 * 30; i++) {
+        let div = document.createElement("div");
+        graph.insertAdjacentElement("beforeend", div);
+        div.addEventListener("mouseover", colorDiv)
+    }
+}
+function largeGrid() {
+    let graph = document.querySelector(".container");
+    graph.innerHTML = "";
+    graph.style.gridTemplateColumns = `repeat(15, 1fr)`;
+    graph.style.gridTemplateRows = `repeat(15, 1fr)`;
+
+    for (let i = 0; i < 15 * 15; i++) {
+        let div = document.createElement("div");
+        graph.insertAdjacentElement("beforeend", div);
+        div.addEventListener("mouseover", colorDiv)
+    }
+}
+
+function colorDiv(){
+    if (color == "black"){
+        this.style.backgroundColor = "black"
+    }
+    if (color == "pink"){
+        this.style.backgroundColor = "pink"
+    }
+    if (color == "blue"){
+        this.style.backgroundColor = "blue"
+    }
+    if (color == "purple"){
+        this.style.backgroundColor = "purple"
+    }
+    if (color == "eraser"){
+        this.style.backgroundColor = "white"
+    }
+    
+}
+
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+
